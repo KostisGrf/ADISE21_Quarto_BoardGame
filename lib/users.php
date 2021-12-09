7b64dec7e43a5a4f3384bf70ef2b64f987a4e4d2
Kostis Grf <kostisgratsos@hotmail.gr>
2021-12-09 05:05:49 +0200

    Creates users

---
+++ lib/users.php
@@ -0,0 +1,62 @@
+<?php
+
+function show_users() {
+	global $mysqli;
+	$sql = 'select username,id from players';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+function show_user($method,$id) {
+	global $mysqli;
+	$sql = 'select username,id from players where id=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('i',$id);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+
+function set_user($input) {
+	$id=0;
+    if(!isset($input['username']) || $input['username']=='') {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"No username given."]);
+		exit;
+	}
+	$username=$input['username'];
+	global $mysqli;
+	$sql = 'select count(*) as c from players where id=1 and username is not null';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	$r = $res->fetch_all(MYSQLI_ASSOC);
+	if($r[0]['c']>0) {
+        $id='2';
+        $sql = 'update players set username=?, token=md5(CONCAT( ?, NOW()))  where id=?';
+	    $st2 = $mysqli->prepare($sql);
+	    $st2->bind_param('ssi',$username,$username,$id);
+	    $st2->execute();
+	}else{
+        $id='1';
+        $sql = 'update players set username=?, token=md5(CONCAT( ?, NOW()))  where id=?';
+	    $st2 = $mysqli->prepare($sql);
+	    $st2->bind_param('ssi',$username,$username,$id);
+	    $st2->execute();
+    }
+	
+
+	update_game_status();
+	$sql = 'select * from players where id=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('i',$id);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+		
+}
+?>
