1d1d5125c15b04e03e4869c28cab84d366f2a60a
Kostis Grf <kostisgratsos@hotmail.gr>
2021-12-20 05:55:09 +0200

    💥Updates models
    [1]Creates function read status
    [2]Creates function current player

--- lib/game.php
+++ lib/game.php
@@ -61,5 +61,16 @@
         $st->bind_param('ss',$new_status,$new_turn);
         $st->execute();
     }
+
+    function read_status()
+{
+	global $mysqli;
+	$sql = 'select * from game_status';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	$status = $res->fetch_assoc();
+	return ($status);
+}
     ?>
 
