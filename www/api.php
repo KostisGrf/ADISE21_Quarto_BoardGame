f15c20e471dadb410e29663a968c88820f2a516b
Kostis Grf <kostisgratsos@hotmail.gr>
2021-12-20 05:51:15 +0200

    ⚡Updates api
    Adds piece endpoint for select piece

--- www/api.php
+++ www/api.php
@@ -4,10 +4,21 @@ require_once "../lib/dbconnect.php";
 require_once "../lib/board.php";
 require_once "../lib/game.php";
 require_once "../lib/users.php";
+require_once "../lib/pieces.php";
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
+
+if($input==null) {
+    $input=[];
+}
+if(isset($_SERVER['HTTP_X_TOKEN'])) {
+    $input['token']=$_SERVER['HTTP_X_TOKEN'];
+} else {
+    $input['token']='';
+}
 
 
 // print "Path_info=".$_SERVER['PATH_INFO']."\n";
@@ -19,7 +30,13 @@ switch ($r=array_shift($request)) {
             case '':
             case null: handle_board($method);
                         break;
+            case 'piece':handle_piece($method, $request[0], $input);
+                        break;
+            default:header("HTTP/1.1 404 Not Found");
+                        break;        
         }
+    case 'piece':handle_piece2($method,$request[0],$input);
+                    break;
 
     case 'status':
         if(sizeof($request)==0) {
@@ -62,23 +79,18 @@ function handle_player($method, $p,$input) {
             print json_encode(['errormesg'=>"Method $method not allowed here."]);}
         
     }else{show_user($method,$b);}
+}
+
+function handle_piece($method, $request, $input){
     
-    // switch ($b=array_shift($p)) {
-	
-    
-    
-    //     //	case '':
-	// //	case null: if($method=='GET') {show_users($method);}
-	// //			   else {header("HTTP/1.1 400 Bad Request"); 
-	// //					 print json_encode(['errormesg'=>"Method $method not allowed here."]);}
-    // //                break;
-    //     case 'B': 
-	// 	case 'W': handle_user($method, $b,$input);
-	// 				break;
-	// 	default: header("HTTP/1.1 404 Not Found");
-	// 			 print json_encode(['errormesg'=>"Player $b not found."]);
-    //              break;
-	// }
+}
+
+function handle_piece2($method,$request,$input){
+    if($request=='select'){
+        if($method=='POST'){
+            select_piece($input);
+        }
+    }
 }
 
 
