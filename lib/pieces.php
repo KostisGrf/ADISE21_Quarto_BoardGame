5b85336552802e2cc64b55af1135083c7a4e6718
KostisGrf <kostisgratsos@hotmail.gr>
2021-12-29 06:16:04 +0200

    💥Updates pieces model
    [1]Added if statements to check if the game is over
    [2]Added if statement to check if the selected piece number is 1-16
    [3]Added statement to check if there is a winner or the game is a draw
    [4]Created readPieceId function

--- lib/pieces.php
+++ lib/pieces.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once "../lib/users.php"; 
-require_once "../lib/game.php"; 
+require_once "../lib/game.php";
+require_once "../lib/board.php";
+
+
 
 function read_available_pieces(){
     header('Content-type: application/json');
@@ -33,6 +36,11 @@ function select_piece($input){
 		print json_encode(['errormesg'=>"Game is not in action."]);
 		exit;
 	}
+    if($status['result']!=null) {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"The game is over."]);
+		exit;
+	}
     if($status['p_turn']!=$player) {
 		header("HTTP/1.1 400 Bad Request");
 		print json_encode(['errormesg'=>"It is not your turn."]);
@@ -43,11 +51,18 @@ function select_piece($input){
 		print json_encode(['errormesg'=>"There is already a selected piece."]);
 		exit;
     }
+    if($input['piece_id']<=0 || $input['piece_id']>16){
+        header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"The piece number must be in the range 1-16"]);
+		exit;
+    }
     if(!in_array($input['piece_id'],$available_pieces)){
         header("HTTP/1.1 400 Bad Request");
 		print json_encode(['errormesg'=>"this piece is not available."]);
 		exit;
-    }    
+    }
+    
+
     do_select($input['piece_id']);
 }
 
@@ -97,20 +112,33 @@ function move_piece($input){
 		print json_encode(['errormesg'=>"Game is not in action."]);
 		exit;
 	}
+    if($status['result']!=null) {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"The game is over."]);
+		exit;
+	}
     if($status['p_turn']!=$player) {
 		header("HTTP/1.1 400 Bad Request");
 		print json_encode(['errormesg'=>"It is not your turn."]);
 		exit;
     }
-
     if($status['selected_piece']==null){
         header("HTTP/1.1 400 Bad Request");
 		print json_encode(['errormesg'=>"You must choose a piece for your opponent."]);
 		exit;
     }
 
-    if(empy_cell($input['x'],$input['y'])){
+    if(empty_cell($input['x'],$input['y'])){
         do_move($input['x'],$input['y']);
+        if(check_win()){
+            setResult($player);
+            exit;
+        }elseif(check_draw()){
+            $draw='d';
+            setResult($draw);
+            exit;
+        }
+        
     }else{
         header("HTTP/1.1 400 Bad Request");
         print json_encode(['errormesg'=>"You cannot place here."]);
@@ -118,7 +146,7 @@ function move_piece($input){
 }
 
 
-function empy_cell($x,$y){
+function empty_cell($x,$y){
     global $mysqli;
 
     $sql='SELECT piece_id FROM board WHERE x=? and y=?';
@@ -140,7 +168,18 @@ function do_move($x,$y){
     $st = $mysqli->prepare($sql);
     $st->bind_param('iii',$selected_piece['selected_piece'],$x,$y);
 	$st->execute();
-}   
+}
+
+function readPieceId($input){
+    global $mysqli;
+
+    $sql = 'select * from pieces where id=?';
+    $st = $mysqli->prepare($sql);
+    $st->bind_param('i',$input['piece_id']);
+	$st->execute();
+    $res = $st->get_result();
+    print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
 
 
 ?>
